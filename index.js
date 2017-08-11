9f9a6ed5d27930f2228ee5701a777bb088c5f9d8
sMorm <sereymorm@gmail.com>
2017-08-10 20:28:29 -0400

    Added IP Tracking for API Requests

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const iplocation = require('iplocation')
 
 // Initialize app
 const app = express();
@@ -39,19 +40,27 @@ app.post('/send', (req, res) => {
 // Endpoint for API
 app.get('/send/:name/:address/:subject/:text', (req, res) => {
   const { name, address, subject, text } = req.params;
-  let mailOptions = {
-    from: `${name} <${address}>`,
-    to: 'receiver-email',
-    subject: subject,
-    text: `Message from: ${address}, ${text}`, 
-  };
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          res.send(error);
-      }
-      res.send(`Successfully sent mail`);
-  });
+
+  iplocation(req.clientIp, (error, response) => {
+    const { ip, country_code, region_name, city, zip_code, longitude, latitude } = response;
+
+    let mailOptions = {
+      from: `${name} <${address}>`,
+      to: 'receiver-email',
+      subject: subject,
+      html: `<html><body> ${text}<br/><br/>Sender Info:<br/>IP:${ip}<br/>${city}, ${region_name} ${country_code}<br/>Exact Location: https://www.google.com/maps/?q=${latitude},${longitude}</body></html>`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.send(error);
+        }
+        res.send(`Successfully sent mail`);
+    });
+  })
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Started listening on port ${PORT}...`)
